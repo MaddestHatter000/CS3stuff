b6d47d9d0b44cce6b2d134cbbd31959ecf8e0e47
MaddestHatter000 <mbish98789@gmail.com>
2025-02-19 11:47:28 -0600

    working on labs

--- u08a_trees/labs/17a_BST/BinarySearchTree.java
+++ u08a_trees/labs/17a_BST/BinarySearchTree.java
@@ -5,6 +5,10 @@
 //Lab  -
 
 //You'll need to supply any missing import statements:
+import static java.lang.System.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class BinarySearchTree
@@ -21,9 +25,16 @@ public class BinarySearchTree
 		root = add(val, root);
 	}
 
+	@SuppressWarnings("unchecked")
 	private TreeNode add(Comparable val, TreeNode tree)
 	{
-
+		if(tree == null)
+			return new TreeNode(val, null, null);
+		if(val.compareTo(tree.getValue()) < 0)
+			tree.setLeft(add(val, tree.getLeft()));
+		if(val.compareTo(tree.getValue()) > 0)
+			tree.setRight(add(val, tree.getRight()));
+		return tree;
 	}
    
    public void inOrder()
@@ -42,6 +53,51 @@ public class BinarySearchTree
 	}
 
 	//add preOrder, postOrder, and revOrder
+	public void preOrder()
+	{
+		preOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void preOrder(TreeNode tree)
+	{
+		if (tree != null){
+			System.out.print(tree.getValue() + " ");
+			preOrder(tree.getLeft());
+			preOrder(tree.getRight());
+		}
+	}
+
+	public void postOrder()
+	{
+		postOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void postOrder(TreeNode tree)
+	{
+		if (tree != null){
+			postOrder(tree.getLeft());
+			postOrder(tree.getRight());
+			System.out.print(tree.getValue() + " ");
+		}
+	}
+
+	public void revOrder()
+	{
+		revOrder(root);
+		System.out.println("\n\n");
+	}
+
+	private void revOrder(TreeNode tree)
+	{
+		if (tree != null){
+			revOrder(tree.getRight());
+			System.out.print(tree.getValue() + " ");
+			revOrder(tree.getLeft());
+		}
+	}
+
 
 
 
@@ -52,29 +108,105 @@ public class BinarySearchTree
 
 	private int getNumLevels(TreeNode tree)
 	{
-		return 0;
+		if(tree == null)
+			return 0;
+		int left = getNumLevels(tree.getLeft());
+		int right = getNumLevels(tree.getRight());
+		return left > right ? 1 + left : 1 + right;
 	}
    
    // Complete the traversals and the toString method
    // and then each of these:
    
-   //clear()
+   public void clear()
+   {
+		root = null;
+   }
 
-	//getNumLeaves 
+   public int getNumLeaves()
+   {
+	   return getNumLeaves(root);
+   }
+
+   private int getNumLeaves(TreeNode tree)
+   {
+	   	if(tree == null)
+		   return 0;
+		if(tree.getLeft() == null && tree.getRight() == null)
+			return 1;
+	   	return getNumLeaves(tree.getLeft()) + getNumLeaves(tree.getRight());
+   }
       
    //getHeight
+   	public int getHeight()
+	{
+		return getHeight(root);
+	}
+	public int getHeight(TreeNode tree)
+	{
+		return getNumLevels(tree)-1;
+	}
+
    
-   //getNumNodes
+  	public int getNumNodes()
+	{
+		return getNumNodes(root);
+	}
+
+	private int getNumNodes(TreeNode tree)
+	{
+		if(tree == null)
+			return 0;
+		return 1 + getNumNodes(tree.getLeft()) + getNumNodes(tree.getRight());
+	}
    
    //isFull
+   	public boolean isFull()
+	{
+		return isFull(root);
+	}
+
+	private boolean isFull(TreeNode tree)
+	{
+		if(tree == null)
+			return false;
+		return isFull(tree.getLeft()) == isFull(tree.getRight());
+	}
+
 	
 	//search
 	
 	//getSmallest
+	public Comparable getSmallest(){
+		TreeNode tree = root;
+		while(tree != null &&tree.getLeft() != null)
+			tree = tree.getLeft();
+		return tree.getValue();
+	}
 	
 	//getLargest
+	public Comparable getLargest(){
+		TreeNode tree = root;
+		while(tree != null &&tree.getRight() != null)
+			tree = tree.getRight();
+		return tree.getValue();
+	}
 		
-   //getWidth
+   	//getWidth
+	   public int getWidth()
+	   {
+		   return getWidth(root);
+	   }
+   
+	   private int getWidth(TreeNode tree)
+	   {
+		   if(tree == null)
+			   return 0;
+		   int myWidth = 1 + getHeight(tree.getLeft())
+			return 0;
+	   }
+
+	
 
 	//remove
 
@@ -84,12 +216,14 @@ public class BinarySearchTree
    // an IN-ORDER traversal of the tree.
 	public String toString()
 	{
-		return "";
+		return toString(root);
 	}
 
 	private String toString(TreeNode tree)
 	{
+		if(tree == null)
 		return "";
+		return toString(tree.getLeft()) + tree.getValue() + toString(tree.getRight());
 	}
 
    public void levelOrder()
--- u08a_trees/labs/17a_BST/Lab17a.java
+++ u08a_trees/labs/17a_BST/Lab17a.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -12,11 +12,20 @@ public class Lab17a
    {
       //Create a BST and add the nodes from the directions in 
       //the correct order to form the tree shown in the directions!!!
-      BinarySearchTree tree;
+      BinarySearchTree tree = new BinarySearchTree();
    
+      tree.add(6);
+      tree.add(3);
+      tree.add(9);
+      tree.add(7);
+      tree.add(2);
+      tree.add(5);
+      tree.add(0);
+      tree.add(10);
+      tree.add(4);
    
-      out.println("LEVEL ORDER");
-      tree.levelOrder();
+      // out.println("LEVEL ORDER");
+      // tree.levelOrder();
         
       out.println("IN ORDER");
       tree.inOrder();
@@ -33,8 +42,8 @@ public class Lab17a
       out.println("\nTree as a string  " + tree.toString());
    
       out.println(tree.isFull()?"\nThe tree is full.":"\nThe tree is not full.");
-      out.println(tree.search(100)?"The tree contains 100!":"The does not contain 100!"); 
-      out.println(tree.search(114)?"The tree contains 114!":"The does not contain 114!");
+      // out.println(tree.search(100)?"The tree contains 100!":"The does not contain 100!"); 
+      // out.println(tree.search(114)?"The tree contains 114!":"The does not contain 114!");
       
       out.println("\nNumber of nodes is " + tree.getNumNodes());
       out.println("\nNumber of leaves is " + tree.getNumLeaves());
@@ -42,41 +51,41 @@ public class Lab17a
       out.println("\nTree height is " + tree.getHeight());
       out.println("The smallest tree node " + tree.getSmallest());   
       out.println("The largest tree node " + tree.getLargest());
-      out.println("\nTree width / diameter is " + tree.getWidth());
+      // out.println("\nTree width / diameter is " + tree.getWidth());
       
       
-      out.println("\n\n\nLevel order traversal is extra credit, but removal is not:");
+      // out.println("\n\n\nLevel order traversal is extra credit, but removal is not:");
                   
-      out.println("\n\n\nTree before removing any nodes - using level order traversal.");
-      tree.levelOrder();
+      // out.println("\n\n\nTree before removing any nodes - using level order traversal.");
+      // tree.levelOrder();
       
-      tree.remove(90);
-      out.println("\nTree after removing 90.");
-      tree.levelOrder();
+      // tree.remove(90);
+      // out.println("\nTree after removing 90.");
+      // tree.levelOrder();
       
-      tree.remove(70);
-      out.println("\nTree after removing 70.");
-      tree.levelOrder();
+      // tree.remove(70);
+      // out.println("\nTree after removing 70.");
+      // tree.levelOrder();
             
-      tree.remove(85);
-      out.println("\nTree after removing 85.");
-      tree.levelOrder();
+      // tree.remove(85);
+      // out.println("\nTree after removing 85.");
+      // tree.levelOrder();
       
-      tree.remove(98);
-      out.println("\nTree after removing 98.");
-      tree.levelOrder();
+      // tree.remove(98);
+      // out.println("\nTree after removing 98.");
+      // tree.levelOrder();
       
-      tree.remove(80);
-      out.println("\nTree after removing 80.");
-      tree.levelOrder();
+      // tree.remove(80);
+      // out.println("\nTree after removing 80.");
+      // tree.levelOrder();
             
-      tree.remove(120); 
-      out.println("\nTree after removing 120.");
-      tree.levelOrder();
+      // tree.remove(120); 
+      // out.println("\nTree after removing 120.");
+      // tree.levelOrder();
             
-      tree.remove(100);                
-      out.println("\nTree after removing 100.");
-      tree.levelOrder();   
+      // tree.remove(100);                
+      // out.println("\nTree after removing 100.");
+      // tree.levelOrder();   
       
       tree.clear();
       tree.levelOrder();
